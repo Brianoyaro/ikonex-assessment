d9009607d257e4462c21e35e9542961faee6f234
Brianoyaro <brianoyaro2000@yahoo.com>
2026-06-06 20:42:36 +0300

    frontend boilerplate

---
+++ ikonex-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className="bg-sky-950 h-64 w-full"></div>
+    </>
+  )
+}
+
+export default App
