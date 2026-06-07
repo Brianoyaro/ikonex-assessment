6d788ac919971f7447d042272abf1a10e2cb1e4e
Brianoyaro <brianoyaro2000@yahoo.com>
2026-06-07 18:35:08 +0000

    added deployed frontend url to cakend CORS options

--- ikonex/src/main/java/com/studentmanagementsystem/ikonex/config/SecurityConfig.java
+++ ikonex/src/main/java/com/studentmanagementsystem/ikonex/config/SecurityConfig.java
@@ -70,7 +70,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "http://localhost:3000"
+                "http://localhost:3000",
+		"https://ikonex-assessment-frontend.onrender.com"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
